c474c40f372637189d3964b2dceb4aa71368d6a6
MoseOH <ohmose1004@gmail.com>
2022-03-12 17:54:29 +0900

    3월 1주차 멘토링 실습

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("Initial commit")
+console.log("Initial commit");
+
+const name = "Mose Oh";
+console.log(name);
+
+// 상수는 재선언 불가
+// 변수는 재선언 가능?
+let age = 16;
+console.log(age);
+
+age = 1912;
+console.log(age);
+
+console.log("22" + 1); // 221
